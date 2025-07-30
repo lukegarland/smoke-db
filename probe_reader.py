b6fbd6ffaaa6fad14f1926b30c70420e3b773993
Luke Garland <luke.garland01@gmail.com>
2025-07-29 18:40:23 -0600

    get device by name (TP25) instead of hard coded addr

--- probe_reader.py
+++ probe_reader.py
@@ -11,8 +11,8 @@ NOTIFY_UUID="1086fff2-3343-4817-8bb2-b32206336ce8"
 
 
 
-
 def callback(sender, data: bytearray):
+    
     print(f"""
 PROBE 1: {int(binascii.hexlify(data[5:7]).decode())/10}
 PROBE 2: {int(binascii.hexlify(data[7:9]).decode())/10}
@@ -20,18 +20,28 @@ PROBE 3: {int(binascii.hexlify(data[9:11]).decode())/10}
 PROBE 4: {int(binascii.hexlify(data[11:13]).decode())/10}
 """)
 
+async def find_device(name="TP25"):
+    devices = await bleak.BleakScanner.discover(timeout=5)
+    for device in devices:
+        print(f"found device {device.name}")
+        if device.name == name:
+            return device
+    print(f"Error. Did not find device with name: {name}")
+    return None
 async def main():
-    addr = "109ACFDA-16E3-E907-F362-6B8CC48DFAE9"
-    
-    client = bleak.BleakClient(address_or_ble_device=addr)
+    device = await find_device()
+    if device: 
+        client = bleak.BleakClient(address_or_ble_device=device)
+        
+        # connect and send command sequence to start streaming / notfiying temperature data
+        await client.connect()
+        await client.write_gatt_char(COMMAND_UUID, bytearray([1,9,62,143,138,108,53,238,38,227,248,241]), response=True)
+        
+        # start temperature notifiactions via callback
+        await client.start_notify(NOTIFY_UUID, callback)
 
-    await client.connect()
-    data = await client.write_gatt_char(COMMAND_UUID, bytearray([1,9,62,143,138,108,53,238,38,227,248,241]), response=True)
-#    print(data)
-    await client.start_notify(NOTIFY_UUID, callback)
-
-    while True:
-        time.sleep(1)
+        while True:
+            await asyncio.sleep(1)
 
 
 asyncio.run(main())
