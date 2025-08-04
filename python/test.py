2ee857b82acbdeabb026c510405f2bf21f2cd4a7
Luke Garland <luke.garland01@gmail.com>
2025-08-04 14:34:06 -0600

    fine tune test script for prediction testing

--- python/test.py
+++ python/test.py
@@ -9,21 +9,21 @@ def main():
     predictor = prometheus.TemperatureTimePredictor(prometheus_exporter)
 
     temperatures = {
-        1: 25,
-        2: 50,
-        3: 65,
-        4: 75
+        1: 10,
+        2: 12.5,
+        3: 45,
+        4: 50
     }
     while True:
 
         for probe_num, temperature in temperatures.items():
             
-            new_temp_delta = random.normalvariate(mu=2, sigma=1)
+            new_temp_delta = random.normalvariate(mu=1.005, sigma=1.1)
             temperature = round(temperature + new_temp_delta,3)
             prometheus_exporter.report_probe_temp(temperature, probe_num)
             temperatures[probe_num] = temperature
             predictor.run_realtime_prediction()
-            
+
         pprint.pprint(temperatures)
         time.sleep(10)
     pass
